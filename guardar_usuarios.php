c2298c7388a812f3eefde69c516af3759cd66270
joaquinamigo356 <elproendragonball@gmail.com>
2024-10-20 11:05:54 -0300

    guardar_usuarios.php

--- guardar_usuarios.php
+++ guardar_usuarios.php
@@ -1,20 +1,17 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Datos de conexión a la base de datos
+if (($_SERVER['REQUEST_METHOD'] ??'') === 'POST') {
     $server = 'localhost';
     $user = 'root';
     $pass = '';
     $db = 'gestion_usuarios';
 
-    // Crear la conexión a la base de datos
     $conn = new mysqli($server, $user, $pass, $db);
 
-    // Verificar la conexión
     if ($conn->connect_error) {
         die("Error de conexión: " . $conn->connect_error);
     }
 
-    // Verificar si se han recibido los datos del formulario
+    
     if (isset($_POST['id'], $_POST['nombre'], $_POST['apellido'], $_POST['correo'], $_POST['perfil'])) {
         $id = $conn->real_escape_string($_POST['id']);
         $nombre = $conn->real_escape_string($_POST['nombre']);
@@ -22,10 +19,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $correo = $conn->real_escape_string($_POST['correo']);
         $perfil = $conn->real_escape_string($_POST['perfil']);
 
-        // Consulta de inserción de datos
         $sql = "INSERT INTO usuarios (id, nombre, apellido, correo, perfil) VALUES ('$id', '$nombre', '$apellido', '$correo', '$perfil')";
 
-        // Ejecutar la consulta e informar sobre el resultado
+    
         if ($conn->query($sql) === TRUE) {
             echo "Usuario registrado exitosamente.";
         } else {
@@ -35,14 +31,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Por favor, completa todos los campos del formulario.";
     }
 
-    // Cerrar la conexión
+   
     $conn->close();
 
-    // Redirigir de nuevo a la página de usuarios
     header("Location: usuarios.html");
     exit();
 } else {
     echo "Solicitud inválida.";
 }
 ?>
-       
