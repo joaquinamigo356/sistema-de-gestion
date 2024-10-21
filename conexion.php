b56214bca0f163e381594a7d716586e447501148
joaquinamigo356 <elproendragonball@gmail.com>
2024-10-21 17:40:26 -0300

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,16 +1,14 @@
 <?php
-// Datos de conexión a la base de datos
+
 $server = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'gestion_usuarios';
 
 function connection(){
-// Crear la conexión
+
 $conn = new mysqli($server, $user, $pass, $db);
 
-
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
