07fdd8589845c94776a0abc9b2cfc553d17a71fd
joaquinamigo356 <elproendragonball@gmail.com>
2024-10-21 17:38:54 -0300

    eliminar_usuarios.php

--- eliminar_usuarios.php
+++ eliminar_usuarios.php
@@ -1,26 +1,22 @@
 <?php
-
- // Verificar si la solicitud es POST
 if (($_SERVER['REQUEST_METHOD'] ??'') === 'POST') {
-    // Configuración de la base de datos
+    
     $server = 'localhost';
     $user = 'root';
     $pass = '';
     $db = 'gestion_usuarios';
 
-    // Incluir la conexión a la base de datos
+    
     include('conexion.php');
 
-    // Escapar el ID recibido
+
     $id = $conn->real_escape_string($_POST['id']);
 
-    // Imprimir el ID del usuario
+    
     echo "ID del usuario: $id";
-
-    // Preparar la consulta SQL para eliminar el usuario
+  
     $sql = "DELETE FROM usuarios WHERE id = $id";
 
-    // Ejecutar la consulta y verificar el resultado
     $resultado = $conn->query($sql);
     if ($resultado === TRUE) {
         echo "Usuario eliminado exitosamente.";
@@ -28,14 +24,13 @@ if (($_SERVER['REQUEST_METHOD'] ??'') === 'POST') {
         echo "Error al eliminar el usuario: " . $conn->error;
     }
 
-    // Imprimir el resultado de la consulta SQL
+    
     echo "Resultado de la consulta SQL: ";
     var_dump($resultado);
 
-    // Cerrar la conexión
+
     $conn->close();
 
-    // Redirigir a la página de usuarios
     header("Location: usuarios.html");
     exit();
 }
