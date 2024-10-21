454246edcaf020a021a090a0b7c387e449aee8fd
joaquinamigo356 <elproendragonball@gmail.com>
2024-10-21 20:38:19 -0300

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    // Obtén los valores ingresados
+    
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Comprobar si username y password son diferentes de vacío
+   
     if (username !== "" && password !== "") {
-        // Redireccionar a 'usuarios.html' para cualquier combinación válida
+        
         document.getElementById("loginMessage").innerText = "Inicio de sesión exitoso!";
         document.getElementById("loginMessage").style.color = "green";
-        window.location.href = "usuarios.html";  // Redirige a 'usuarios.html'.
+        window.location.href = "usuarios.html";  
     } else {
         document.getElementById("loginMessage").innerText = "Por favor, ingresa tus credenciales.";
         document.getElementById("loginMessage").style.color = "red";
     }
 
-    // Comprobaciones específicas para tecnico y admin
+    
     if (username === "tecnico" && password === "5678") {
         document.getElementById("loginMessage").innerText = "Inicio de sesión exitoso como Técnico!";
         document.getElementById("loginMessage").style.color = "green";
-        window.location.href = "mantenimiento.html";  // Redirige a 'mantenimiento.html'.
-        return; // Detener ejecución para evitar redirección a 'usuarios.html'
+        window.location.href = "mantenimiento.html";  
+        return; 
     } 
     if (username === "admin" && password === "1234") {
         document.getElementById("loginMessage").innerText = "Inicio de sesión exitoso como Admin!";
         document.getElementById("loginMessage").style.color = "green";
-        window.location.href = "index.html";  // Redirige a 'index.html'.
-        return; // Detener ejecución para evitar redirección a 'usuarios.html'
+        window.location.href = "index.html";  
+        return; 
     } 
 
-    // Si el username y password no son válidos
+   
     if (username === "" || password === "") {
         document.getElementById("loginMessage").innerText = "Por favor, completa ambos campos.";
         document.getElementById("loginMessage").style.color = "red";
